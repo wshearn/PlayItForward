2eccd3b1331124106ea092626601ca662443fa0b
Robert Baker <se7enalive@gmail.com>
2012-06-04 13:15:51 -0700

    Removed unused usings.

--- Models/PiFGame.cs
+++ Models/PiFGame.cs
@@ -2,11 +2,8 @@
 // <license href="http://www.gnu.org/licenses/gpl-3.0.txt" name="GNU General Public License 3" />
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Runtime.Serialization;
 
 namespace PiF.Models
 {
